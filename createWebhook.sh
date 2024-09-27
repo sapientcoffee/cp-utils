15eb3bd2fd2bf086ed588562bbc24e01a4b999da
Rob Edwards <rob.j.edwards@gmail.com>
2024-09-27 12:51:00 -0700

    Update createWebhook.sh

--- createWebhook.sh
+++ createWebhook.sh
@@ -24,8 +24,8 @@ export GIT_BASE_URL=https://${GIT_USER}@github.com/${GIT_USER}
 
 export APP_INSTANCE_REPO_LOCATION=https://github.com/${GIT_USERNAME}/${APP_ID}
 
-export IMAGE_REPO=gcr.io/${PROJECT_ID}
-export PROJECT_NUMBER=$(gcloud projects describe $PROJECT_ID --format='value(projectNumber)')
+export IMAGE_REPO=gcr.io/${GOOGLE_CLOUD_PROJECT}
+export PROJECT_NUMBER=$(gcloud projects describe $GOOGLE_CLOUD_PROJECT --format='value(projectNumber)')
 
 export SECRET_NAME=${APP_ID}-webhook-trigger-secret
 
@@ -38,7 +38,8 @@ printf ${SECRET_VALUE} | gcloud secrets create ${SECRET_NAME} --data-file=-
 
 gcloud secrets add-iam-policy-binding ${SECRET_NAME} \
     --member=serviceAccount:service-${PROJECT_NUMBER}@gcp-sa-cloudbuild.iam.gserviceaccount.com \
-    --role='roles/secretmanager.secretAccessor'
+    --role='roles/secretmanager.secretAccessor' \
+    --project=${GOOGLE_CLOUD_PROJECT}
 
 ## Create CloudBuild Webhook Endpoint
 echo Create CloudBuild Webhook Endpoint
@@ -52,10 +53,11 @@ gcloud alpha builds triggers create webhook \
     --inline-config=$BUILD_YAML_PATH \
     --secret=${SECRET_PATH} \
     --substitutions="_APP_ID=${APP_ID},_REGION=${REGION}"',_APP_REPO=$(body.repository.html_url),_REF=$(body.ref),_SHA=$(body.after)' \
-    --subscription-filter="_REF != '\$(body.ref)'"
+    --subscription-filter="_REF != '\$(body.ref)'" \
+    --project=${GOOGLE_CLOUD_PROJECT}
 
 ## Retrieve the URL 
-WEBHOOK_URL="https://cloudbuild.googleapis.com/v1/projects/${PROJECT_ID}/triggers/${TRIGGER_NAME}:webhook?key=${API_KEY_VALUE}&secret=${SECRET_VALUE}"
+WEBHOOK_URL="https://cloudbuild.googleapis.com/v1/projects/${GOOGLE_CLOUD_PROJECT}/triggers/${TRIGGER_NAME}:webhook?key=${API_KEY_VALUE}&secret=${SECRET_VALUE}"
 echo WEBHOOK_URL=${WEBHOOK_URL}
 
 ## Configure Github Repo Webhook
